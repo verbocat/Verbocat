6043d9ce6bc45b2143e030cb933635134cc603f4
divyanshu22299 <divyanshusingh2229@gmail.com>
2026-06-04 17:24:09 +0530

    fix: build syntax error in GlossaryModal

--- client/src/components/GlossaryModal.jsx
+++ client/src/components/GlossaryModal.jsx
@@ -324,7 +324,8 @@ export const GlossaryModal = ({
                         />
                       </div>
                     );
-                  })}
+                  })
+                  )}
                 </div>
               </div>
             </div>
